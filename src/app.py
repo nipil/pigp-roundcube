0dc41198a37930bd473b611383bd940dedb3d49d
nipil <nipil@users.noreply.github.com>
2017-03-04 13:31:46 +0100

    accept incoming connections

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import selectors
 import signal
 import socket
 
@@ -15,6 +16,7 @@ class Main:
         Main.Arguments = None
         self.listen_sock_v4 = None
         self.listen_sock_v6 = None
+        self.listen_selector = None
         # signal handling
         signal.signal(signal.SIGTERM, signal_handler)
         signal.signal(signal.SIGINT, signal_handler)
@@ -43,11 +45,21 @@ class Main:
     def create_listen_socket(self, address_family, bind_address):
         logging.debug("creating socket of family {0}".format(address_family))
         sock = socket.socket(address_family, socket.SOCK_STREAM)
+        logging.debug("allowing address reuse for socket {0}".format(sock))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         logging.debug("socket {0} created, binding it to {1} on TCP port {2}".format(sock.fileno(), bind_address, Main.Arguments.listen_port))
         sock.bind((bind_address, Main.Arguments.listen_port))
         logging.debug("setting listen queue to {1} on socket {0}".format(sock.fileno(), Main.Arguments.max_wait_conn))
         sock.listen(Main.Arguments.max_wait_conn)
         return sock
+
+    def accept_incoming_connection(self, fileobj, mask):
+        logging.debug("accepting incoming connection on socket {0}".format(fileobj))
+        client_sock, client_addr = fileobj.accept()
+        logging.debug("client socket {0} connected from {1} using source port {2}".format(client_sock, *client_addr))
+        client_sock.shutdown(socket.SHUT_RDWR)
+        client_sock.close()
+        logging.debug("client connection closed")
 
     def run(self):
         logging.info("starting applicaiton")
@@ -55,15 +67,47 @@ class Main:
         if Main.Arguments.listen_ipv4:
             self.listen_sock_v4 = self.create_listen_socket(socket.AF_INET, Main.Arguments.listen_ipv4)
             logging.debug("listening ipv4 socket is {0}".format(self.listen_sock_v4))
+            # self.listen_sock_v4.settimeout(False)
+            # logging.debug("listening ipv4 socket set to non-blocking")
         if Main.Arguments.listen_ipv6:
             self.listen_sock_v6 = self.create_listen_socket(socket.AF_INET6, Main.Arguments.listen_ipv6)
             logging.debug("listening ipv6 socket is {0}".format(self.listen_sock_v6))
+            # self.listen_sock_v6.settimeout(False)
+            # logging.debug("listening ipv6 socket set to non-blocking")
         if not self.listen_sock_v4 and not self.listen_sock_v6:
             raise Exception("No listening socket created")
+        # setup selector
+        self.listen_selector = selectors.DefaultSelector()
+        logging.debug("socket selector {0} created".format(self.listen_selector))
+        if self.listen_sock_v4:
+            logging.debug("registering listening ipv4 socket into socket selector")
+            self.listen_selector.register(self.listen_sock_v4, selectors.EVENT_READ, self.accept_incoming_connection)
+        if self.listen_sock_v6:
+            logging.debug("registering listening ipv6 socket into socket selector")
+            self.listen_selector.register(self.listen_sock_v6, selectors.EVENT_READ, self.accept_incoming_connection)
         # accept loop
         while not Main.RequestExit:
-            pass
+            # logging.debug("begin while")
+            events = self.listen_selector.select(1)
+            # logging.debug("events={0}".format(events))
+            for key, mask in events:
+                # logging.debug("begin for with key={0} and mask={1}".format(key, mask))
+                callback = key.data
+                # logging.debug("callback is {0} with fileobj={1}".format(callback, key.fileobj))
+                callback(key.fileobj, mask)
+                # logging.debug("end for")
+            # logging.debug("end while")
+        # teardown selector
+        if self.listen_sock_v4:
+            logging.debug("unregistering listening ipv4 socket from socket selector")
+            self.listen_selector.unregister(self.listen_sock_v4)
+        if self.listen_sock_v6:
+            logging.debug("unregistering listening ipv6 socket from socket selector")
+            self.listen_selector.unregister(self.listen_sock_v6)
+        logging.debug("closing socket selector {0}".format(self.listen_selector))
+        self.listen_selector.close()
         # close listening sockets
+        self.listen_selector = selectors.DefaultSelector()
         if self.listen_sock_v4:
             self.listen_sock_v4.shutdown(socket.SHUT_RDWR)
             self.listen_sock_v4.close()
